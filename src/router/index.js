76c4bb1922c0e5632ddbf4a3cde18f52ef0aaef8
Vito <zh.1122521@163.com>
2019-07-13 11:28:24 +0800

    路由组件配置完成

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// 导入vue对象
+import Vue from 'vue'
+// 导入vuerouter对象
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+// 实例化
+const router = new VueRouter({
+  // 配置路由规则
+  routes: []
+})
+// 导出router
+export default router
